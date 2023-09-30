1ac704e4d8a534326d6dfd9c1b3c0c37b72f9f77
Сергей <fserg2003@gmail.com>
2023-09-30 17:22:35 +0300

    Revert "Added second button with color changing functionality"
    This reverts commit 6515f035427f753c09947bb3f6ce6b6d8842adb0.

--- app/src/main/java/com/example/lab0918/MainActivity.java
+++ app/src/main/java/com/example/lab0918/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.lab0918;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -21,9 +20,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnChangeTextClick(View view) {
         this.tvMain.setText("Changed text");
-    }
-
-    public void button2Click(View view) {
-        this.tvMain.setTextColor(Color.rgb(200,0,0));
     }
 }
